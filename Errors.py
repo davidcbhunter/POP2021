8207288f0a0a73a7b722a0ed47f2e25629795902
davidcbhunter <hunter.davidcb@gmail.com>
2021-05-18 14:11:07 +0900

    Add files via upload

---
+++ Errors.py
@@ -0,0 +1,40 @@
+import datetime
+
+id = 87754345872
+patient_name = "David Hunter"
+
+#This is an error! これがまちがい！
+#strange = id + patient_name
+
+# birthday
+#NOT GOOD!
+#date_of_birth_year = 2021
+#date_of_birth_month = "January"
+#date_of_birth_day = 1
+date_of_birth = datetime.date(2021,1,1)
+
+#printing
+#print(date_of_birth)
+#print(type(date_of_birth))
+#year = date_of_birth.year
+#print(year)
+#print(type(year))
+#month = date_of_birth.month
+#print(month)
+#print(type(month))
+
+# gender
+gender = "Male"
+
+
+# nationality
+nationality = "American"
+
+
+#has_insurance
+has_insurance = True
+
+
+#current_illness
+current_illness = None
+
