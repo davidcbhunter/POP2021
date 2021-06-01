5acdc37d9b5c978eccece977579ccda9be4c1267
davidcbhunter <hunter.davidcb@gmail.com>
2021-06-01 14:21:05 +0900

    Add files via upload

--- if.py
+++ if.py
@@ -1,21 +1,39 @@
+import datetime
 fruit = input("What is your favorite fruit? \n")
+fruits = ["apple","orange","banana",\
+          "pineapple","grape","grapefruit"]
 
-if fruit == "apple":
-    print("I like apples, too")
-elif fruit == "orange":
-    print("I like oranges, too")
-elif fruit == "pineapple":
-    print("I like pineapples, too")
-else:
-    print("I don't know that fruit")
+#print(type(fruits))
+#print(type(fruits[0]))
+#print(type(fruits[-1]))
 
-if fruit == "apple":
-    print("I like apples, too")
+#length
+#print(len(fruits))
+
+if fruit in fruits:
+    #print("I know that fruit!!")
+    print("I like " + fruit + "s, too.")
 else:
-    if fruit == "orange":
-        print("I like oranges, too")
-    else:
-        if fruit == "pineapple":
-            print("I like pineapples, too")
-        else:
-            print("I don't know that fruit")
+    print("I don't know that fruit!")
+    fruits.append(fruit)
+    print(fruits)
+
+#if fruit == "apple":
+#    print("I like apples, too")
+#elif fruit == "orange":
+#    print("I like oranges, too")
+#elif fruit == "pineapple":
+#    print("I like pineapples, too")
+#else:
+#    print("I don't know that fruit")
+
+#if fruit == "apple":
+#    print("I like apples, too")
+#else:
+#    if fruit == "orange":
+#        print("I like oranges, too")
+#    else:
+#        if fruit == "pineapple":
+#            print("I like pineapples, too")
+#        else:
+#            print("I don't know that fruit")
