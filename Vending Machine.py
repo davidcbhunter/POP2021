b5fcc4ab81e19b1da4362f41dc704c726e1e558c
davidcbhunter <hunter.davidcb@gmail.com>
2021-06-08 14:31:21 +0900

    Add files via upload

--- Vending Machine.py
+++ Vending Machine.py
@@ -16,25 +16,51 @@ cost = 200
 def greet_customer():
     print("Welcome to " + name + "\n")
 
+def show_products():
+    for x in products:
+        print(x,end = "\t")
+
+def show_prices():
+    for x in prices:
+        print(x,end = "\t")
+
+def show_selection_number():
+    for x in range(len(products)):
+        print(x+1,end = "\t")
+
 greet_customer()
 
-#customer_money = int(input("Please enter some money \n"))
+show_products()
+
+print("\n")
+show_prices()
+
+print("\n")
+
+show_selection_number()
+
+print("\n")
+
 customer_money = input("Please enter some money \n")
 
 if customer_money.isdigit():
     customer_money = int(customer_money)
-    if customer_money >= cost:
-        inventory -= 1
-        change = customer_money - cost
-        money += cost
-        print("Your change is " + str(change))
-        print("We have " + str(inventory) + product)
-        print("Thank you")
-    else:
-        #fix this!!!
-        print("Not enough money")
-
-#money = money + customer_money
-
-#print(customer_money)
-#print(type(customer_money))
+    customer_product = input("Please enter product number")
+    if customer_product.isdigit():
+        customer_product = int(customer_product)
+        if customer_product > 0 and \
+           customer_product <= len(products):
+            #check the cost and inventory
+            if customer_money >= prices[customer_product-1]:
+                stocks[customer_product-1] -= 1
+                change = customer_money - \
+                         prices[customer_product-1]
+                money += prices[customer_product-1]
+                print("Your change is " + str(change))
+                print("We have " + \
+                      str(stocks[customer_product-1]) \
+                      + products[customer_product-1])
+                print("Thank you")
+            else:
+                #fix this!!!
+                print("Not enough money")
---
+++ for.py
@@ -0,0 +1,12 @@
+#for x in range(50,100):
+#    print(x)
+
+
+sweets = ["monblan","macaron",\
+          "cake","omochi",\
+          "sabayon" ]
+
+#print(len(sweets))
+for x in sweets:
+    print(len(len(x)))
+    
