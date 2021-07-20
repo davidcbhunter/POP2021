dcce266550912c7544b0e2ec52aaef28a9f56dfc
davidcbhunter <hunter.davidcb@gmail.com>
2021-07-20 14:17:01 +0900

    Add files via upload

---
+++ student.py
@@ -0,0 +1,47 @@
+pop = ["Nazuna","Kokono","Shu","Miyu","David"]
+
+#print(pop[0])
+#print(pop[1])
+#print(pop[2])
+#print(pop[3])
+#print(pop[4])
+#print(pop[5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#student = 0
+#print(pop[student])
+#student += 1
+#print(pop[student])
+#student += 1
+#print(pop[student])
+#student += 1
+#print(pop[student])
+#student += 1
+#print(pop[student])
+
+
+
+
+
+#for student in range(len(pop)):
+#    print(pop[student])
+
+
+for student in pop:
+    print(student)
+
+#for x in range(40):
+#    print(x)
