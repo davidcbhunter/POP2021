78140279665b1a349e4cf69ef84a379c115de326
davidcbhunter <hunter.davidcb@gmail.com>
2022-02-15 13:39:17 +0900

    Add files via upload

---
+++ Review.py
@@ -0,0 +1,104 @@
+# reviewing different types/classes
+
+a = 10500
+
+
+
+
+
+
+
+
+
+
+
+
+
+b = a - 101
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+c = b < a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+d = "True"
+
+
+
+
+
+
+
+
+
+
+e = d + "?"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+f = True
+
+
+class Job:
+    def __init__(self, n, s):
+        self.name = n
+        self.salary = s
+
+job = Job("Doctor",20000000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if review
+i = "I"
+j = i == "i"
+
+print(type(j))
+
+
+if i == "i":
+    print("same")
